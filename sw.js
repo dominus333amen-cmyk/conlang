2d509f34513b5c29706b8d21b4b40bb281df44f7
dominus333amen-cmyk <dominus333amen@gmail.com>
2026-08-18 19:38:28 +0800

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,31 @@
+var CACHE='tansica-v1';
+var ASSETS=['./','index.html','manifest.webmanifest','icon.svg'];
+
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }));
+});
+
+self.addEventListener('fetch',function(e){
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    caches.match(e.request).then(function(hit){
+      if(hit)return hit;
+      return fetch(e.request).then(function(resp){
+        if(resp.ok&&e.request.url.startsWith(self.location.origin)){
+          var copy=resp.clone();
+          caches.open(CACHE).then(function(c){c.put(e.request,copy);});
+        }
+        return resp;
+      }).catch(function(){
+        if(e.request.mode==='navigate')return caches.match('index.html');
+      });
+    })
+  );
+});
